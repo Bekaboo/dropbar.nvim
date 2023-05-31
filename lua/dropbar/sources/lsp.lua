55d53f662a2dd0effbf4938eaee332840a8fdb55
Zeng <76579810+Bekaboo@users.noreply.github.com>
2023-05-31 08:27:05 -0500

    feat(sources): lsp: check nil before indexing info.data.client_id

--- lua/dropbar/sources/lsp.lua
+++ lua/dropbar/sources/lsp.lua
@@ -405,8 +405,7 @@ local function init()
     desc = 'Attach LSP symbol getter to buffer when an LS that supports documentSymbol attaches.',
     group = groupid,
     callback = function(info)
-      local client_id = (info.data or {}).client_id
-      local client = vim.lsp.get_client_by_id(client_id)
+      local client = vim.lsp.get_client_by_id(info.data and info.data.client_id)
       if client and client.supports_method('textDocument/documentSymbol') then
         attach(info.buf)
       end
