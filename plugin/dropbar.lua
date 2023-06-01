f96d260a40fc03293ca90929de13efd9ec841cf1
Null Chilly <nullchilly@gmail.com>
2023-06-01 16:05:51 +0700

    feat: notify neovim version requirement

--- plugin/dropbar.lua
+++ plugin/dropbar.lua
@@ -1,3 +1,8 @@
+if 1 ~= vim.fn.has('nvim-0.10.0') then
+  vim.api.nvim_err_writeln('dropbar.nvim requires at least nvim-0.10.0')
+  return
+end
+
 vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, {
   once = true,
   group = vim.api.nvim_create_augroup('DropBarSetup', {}),
