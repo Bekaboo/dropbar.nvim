74ebfb70af7c372c3f391542f168e1825257e5c2
Theo Fabi <92238946+theofabilous@users.noreply.github.com>
2023-07-18 13:04:29 -0400

    Support background highlight (#51)
    * feat(utils): hl: use vim.highlight.range to control highlight priorities
    
    - in range_single(), use vim.highlight.range() instead of
      nvim_buf_add_highlight() because the former uses extmarks under the
      hood which permits controlling highlight priorities
    - range_single() now also accepts an optional priority argument,
      defaults to vim.highlight.priorities.user + 1
    
    * feat(hlgroups/bar): apply hl-WinBar background to dropbar if set
    
    - initialize the DropBarKind... highlights in hlgroups.set_hlgroups() if
      a background is set, so that their background fields can be set
    - reorder highlight merge arguments in bar.lua when temporarily
      overriding highlight groups for hover/context such that hover/context
      highlights take precedence (eg.: prior to this change, if a background
      is set and hover or context is Visual, which has a bg highlight
      attribute, this wouldn't be visible in the dropbar because its fields
      were not prioritized)
    
    * feat(menu): hover/context correctly highlighted if background set
    
    Use vim.highlight.range() in dropbar_menu_t:add_hl with a priority level
    one less than the default value used in utils.hl.range_..() functions in
    the utils module. This allows the hover/context highlights to override
    highlights correctly. For example, prior to this commit, menu items with
    default highlights that had a background attribute would not have their
    background colors updated when the hover/context highlights updated.
    
    * feat(sources.path,hlgroups,utils.hl): apply background to devicons
    
    - Respect background highlight on dropbar for devicons
    - Creates a new highlight group for each devicon highlight group when
      first encountered with only the background attribute changed, and
      cached in a table. Cleared if set_hlgroups() is called. If no
      background is set, no new highlights are created
    - utils.hl.merge() now accepts table arguments which are interpreted as
      highlight attribute tables as if already obtained thru nvim_get_hl()
    
    * feat(bar,menu,hlgroups): support all WinBar and WinBarNC hl attributes
    
    * feat(hlgroups): manage non-dropbar defined highlights
    
    - hlgroups.managed() can be used to create a highlight group from its
      input which will be kept in sync with WinBar(NC) highlights
    
    * fix(hlgroups): don't clear devicon highlight groups in set_hlgroups()
    
    - They are being overridden in that function anyway. Clearing the
      devicon highlights causes some of them to lose their highlight colors
      temporarily after the first buffer is written.
    
    * fix(hlgroups): error if WinBar highlight groups empty (#62)

--- lua/dropbar/hlgroups.lua
+++ lua/dropbar/hlgroups.lua
@@ -1,3 +1,5 @@
+local utils = require('dropbar.utils')
+
 -- stylua: ignore start
 local hlgroups = {
   DropBarCurrentContext            = { link = 'Visual' },
@@ -77,26 +79,206 @@ local hlgroups = {
   DropBarMenuNormalFloat           = { link = 'NormalFloat' },
   DropBarPreview                   = { link = 'Visual' },
 }
+
+local kinds = {
+  "DropBarKindArray",
+  "DropBarKindBoolean",
+  "DropBarKindBreakStatement",
+  "DropBarKindCall",
+  "DropBarKindCaseStatement",
+  "DropBarKindClass",
+  "DropBarKindConstant",
+  "DropBarKindConstructor",
+  "DropBarKindContinueStatement",
+  "DropBarKindDeclaration",
+  "DropBarKindDelete",
+  "DropBarKindDoStatement",
+  "DropBarKindElseStatement",
+  "DropBarKindEnum",
+  "DropBarKindEnumMember",
+  "DropBarKindEvent",
+  "DropBarKindField",
+  "DropBarKindFile",
+  "DropBarKindFolder",
+  "DropBarKindForStatement",
+  "DropBarKindFunction",
+  "DropBarKindH1Marker",
+  "DropBarKindH2Marker",
+  "DropBarKindH3Marker",
+  "DropBarKindH4Marker",
+  "DropBarKindH5Marker",
+  "DropBarKindH6Marker",
+  "DropBarKindIdentifier",
+  "DropBarKindIfStatement",
+  "DropBarKindInterface",
+  "DropBarKindKeyword",
+  "DropBarKindList",
+  "DropBarKindMacro",
+  "DropBarKindMarkdownH1",
+  "DropBarKindMarkdownH2",
+  "DropBarKindMarkdownH3",
+  "DropBarKindMarkdownH4",
+  "DropBarKindMarkdownH5",
+  "DropBarKindMarkdownH6",
+  "DropBarKindMethod",
+  "DropBarKindModule",
+  "DropBarKindNamespace",
+  "DropBarKindNull",
+  "DropBarKindNumber",
+  "DropBarKindObject",
+  "DropBarKindOperator",
+  "DropBarKindPackage",
+  "DropBarKindPair",
+  "DropBarKindProperty",
+  "DropBarKindReference",
+  "DropBarKindRepeat",
+  "DropBarKindScope",
+  "DropBarKindSpecifier",
+  "DropBarKindStatement",
+  "DropBarKindString",
+  "DropBarKindStruct",
+  "DropBarKindSwitchStatement",
+  "DropBarKindType",
+  "DropBarKindTypeParameter",
+  "DropBarKindUnit",
+  "DropBarKindValue",
+  "DropBarKindVariable",
+  "DropBarKindWhileStatement",
+}
 -- stylua: ignore end
 
----Set winbar highlight groups
----@return nil
-local function set_hlgroups()
-  for hl_name, hl_settings in pairs(hlgroups) do
-    hl_settings.default = true
-    vim.api.nvim_set_hl(0, hl_name, hl_settings)
+local M = {
+  namespaces = {
+    current = vim.api.nvim_create_namespace('DropBarCurrent'),
+    nc = 0,
+    menu = vim.api.nvim_create_namespace('DropBarMenu'),
+  },
+  dropbar = {
+    ---@type table<string, any>?
+    current = nil,
+    ---@type table<string, any>?
+    nc = nil,
+    ---@type table<string, any>?
+    menu = nil,
+  },
+  ---@type table<string, boolean>
+  devicons = {},
+  ---@type table<string, boolean>
+  managed = {},
+}
+
+---Create a highlight group with name `hl_name` and the given `hl_info`
+---for each namespace
+---@param hl_name string The name of the highlight group to create
+---@param hl_info table<string, any>|string highlight group name to link or highlight attribute table
+local function create_hl(hl_name, hl_info)
+  hl_info = utils.hl.normalize(hl_info, true)
+  for _, e in ipairs({ 'current', 'nc', 'menu' }) do
+    vim.api.nvim_set_hl(
+      M.namespaces[e],
+      hl_name,
+      utils.hl.merge(hl_info, M.dropbar[e])
+    )
   end
 end
 
+---Get the patched highlight group for the given devicon highlight group or
+---the given highlight group if no background color is set
+---@param hlgroup string The highlight group to patch returned by get_icon()
+---@return string highlight group name
+function M.get_devicon_hlgroup(hlgroup)
+  local new_hlgroup = 'DropBar' .. hlgroup
+  if M.devicons[new_hlgroup] then
+    return new_hlgroup
+  end
+  create_hl(new_hlgroup, hlgroup)
+  M.devicons[new_hlgroup] = true
+  return new_hlgroup
+end
+
+---Register a non-dropbar defined highlight group to be managed by dropbar,
+---for example when setting icon/name highlights for symbols
+---@param hlgroup string
+---@return string: The managed highlight group name
+function M.manage(hlgroup)
+  M.managed[hlgroup] = true
+  local managed_hlgroup = 'DropBarManaged' .. hlgroup
+  create_hl(managed_hlgroup, hlgroup)
+  return managed_hlgroup
+end
+
+---Set winbar highlight groups and override background if needed
+---@return nil
+function M.set_hlgroups()
+  M.dropbar.current = utils.hl.without('WinBar', { 'fg', 'nocombine' })
+  M.dropbar.nc = utils.hl.without('WinBarNC', { 'fg', 'nocombine' })
+  M.dropbar.menu = utils.hl.without('WinBar', {
+    'fg',
+    'bold',
+    'standout',
+    'underline',
+    'undercurl',
+    'underdouble',
+    'underdotted',
+    'underdashed',
+    'strikethrough',
+    'italic',
+    'reverse',
+    'nocombine',
+  })
+
+  -- hack: set WinBarNC to WinBar in the namespace 'current' so that
+  -- the dropbar is highlighted as if it is the current one even when inside a
+  -- dropbar_menu_t
+  vim.api.nvim_set_hl(M.namespaces.current, 'WinBarNC', M.dropbar.current)
+
+  --- list of hlgroups that should not be overridden
+  local ignore = {
+    'DropBarCurrentContext',
+    'DropBarMenuCurrentContext',
+    'DropBarHover',
+    'DropBarMenuHoverEntry',
+    'DropBarMenuHoverIcon',
+    'DropBarMenuHoverSymbol',
+    'DropBarPreview',
+  }
+
+  for hl_name, hl_info in pairs(hlgroups) do
+    if vim.tbl_contains(ignore, hl_name) then
+      hl_info.default = true
+      vim.api.nvim_set_hl(0, hl_name, hl_info)
+    else
+      create_hl(hl_name, hl_info)
+    end
+  end
+
+  for _, kind in ipairs(kinds) do
+    create_hl(kind, {})
+  end
+
+  for k, _ in pairs(M.managed) do
+    M.manage(k)
+  end
+
+  vim.api.nvim_set_hl(
+    M.namespaces.menu,
+    'NormalFloat',
+    { link = 'DropBarMenuNormalFloat', default = true }
+  )
+  vim.api.nvim_set_hl(
+    M.namespaces.menu,
+    'FloatBorder',
+    { link = 'DropBarMenuFloatBorder', default = true }
+  )
+end
+
 ---Initialize highlight groups for dropbar
-local function init()
-  set_hlgroups()
+function M.init()
+  M.set_hlgroups()
   vim.api.nvim_create_autocmd('ColorScheme', {
     group = vim.api.nvim_create_augroup('DropBarHlGroups', {}),
-    callback = set_hlgroups,
+    callback = M.set_hlgroups,
   })
 end
 
-return {
-  init = init,
-}
+return M
