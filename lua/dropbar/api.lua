c266893cc424f47a053dfd135aece7e6988898d8
Theo Fabi <fabi.theo@gmail.com>
2023-07-31 12:39:45 -0400

    feat(api): add fuzzy finding api functions

--- lua/dropbar/api.lua
+++ lua/dropbar/api.lua
@@ -1,3 +1,5 @@
+local configs = require('dropbar.configs')
+
 ---Get the dropbar
 ---@param buf? integer
 ---@param win integer
@@ -89,6 +91,62 @@ local function pick(idx)
   end
 end
 
+---Toggle fuzzy finding in current dropbar menu
+---@param opts table? fuzzy find options, ignored if closing fuzzy find
+local function fuzzy_find_toggle(opts)
+  local menu = get_current_dropbar_menu()
+  if not menu then
+    return
+  end
+  if menu.fzf_state then
+    menu:fuzzy_find_close(true)
+  else
+    menu:fuzzy_find_open(opts)
+  end
+end
+
+---Click on the currently selected fuzzy find menu entry, choosing the component
+---to click according to `component`.
+---
+---If `component` is a `number`, the `component`-nth symbol is selected, unless
+---`0` or `-1` is supplied, in which case the *first* or *last* clickable component
+---is selected, respectively. If it is a `function`, it receives the `dropbar_menu_entry_t`
+---as an argument and should return the `dropbar_symbol_t` that is to be clicked.
+---@param component? number|dropbar_symbol_t|fun(entry: dropbar_menu_entry_t):dropbar_symbol_t?
+local function fuzzy_find_click(component)
+  local menu = get_current_dropbar_menu()
+  if not menu or not menu.fzf_state then
+    return
+  end
+  menu:fuzzy_find_click_on_entry(component)
+end
+
+---Select the previous/next entry in the menu while fuzzy finding
+---@param direction "up" | "down"
+local function fuzzy_find_navigate(direction)
+  local menu = get_current_dropbar_menu()
+  if not menu or not menu.fzf_state then
+    return
+  end
+  local line_count = vim.api.nvim_buf_line_count(menu.buf)
+  if line_count <= 1 then
+    return
+  end
+  local cursor = vim.api.nvim_win_get_cursor(menu.win)
+  if direction == 'up' then
+    cursor[1] = math.max(1, cursor[1] - 1)
+  elseif direction == 'down' then
+    cursor[1] = math.min(line_count, cursor[1] + 1)
+  else
+    return
+  end
+  vim.api.nvim_win_set_cursor(menu.win, cursor)
+  menu:update_hover_hl(cursor)
+  if configs.opts.menu.preview then
+    menu:preview_symbol_at(cursor)
+  end
+end
+
 return {
   get_dropbar = get_dropbar,
   get_current_dropbar = get_current_dropbar,
@@ -97,4 +155,7 @@ return {
   goto_context_start = goto_context_start,
   select_next_context = select_next_context,
   pick = pick,
+  fuzzy_find_toggle = fuzzy_find_toggle,
+  fuzzy_find_click = fuzzy_find_click,
+  fuzzy_find_navigate = fuzzy_find_navigate,
 }
