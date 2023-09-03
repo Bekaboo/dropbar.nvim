d89022eebcd9236c62112e27984c474c2124d864
bekaboo <18127878294@qq.com>
2023-09-03 18:18:30 +0800

    refactor(sources): terminal: remove usage of deprecated api; simplify logic

--- lua/dropbar/sources/terminal.lua
+++ lua/dropbar/sources/terminal.lua
@@ -1,23 +1,14 @@
-local initialized
-local configs
+local bar = require('dropbar.bar')
+local menu = require('dropbar.menu')
+local configs = require('dropbar.configs')
+local utils = require('dropbar.utils')
 
-local function init()
-  initialized = true
-  configs = require('dropbar.configs')
-end
-
+---@param buf integer buffer handler
+---@return dropbar_symbol_opts_t
 local function buf_info(buf)
-  local icon = configs.opts.sources.terminal.icon
-  if type(icon) == 'function' then
-    icon = icon(buf)
-  end
-  local name = configs.opts.sources.terminal.name
-  if type(name) == 'function' then
-    name = name(buf)
-  end
   return {
-    icon = icon,
-    name = name,
+    icon = configs.eval(configs.opts.sources.terminal.icon, buf),
+    name = configs.eval(configs.opts.sources.terminal.name, buf),
     icon_hl = 'DropBarIconKindTerminal',
   }
 end
@@ -37,45 +28,42 @@ local function get_menu_entries(sym)
         vim.api.nvim_win_set_buf(sym.bar.win, buf)
         sym.menu:close(true)
       end
-      return require('dropbar.menu').dropbar_menu_entry_t:new({
+      return menu.dropbar_menu_entry_t:new({
         components = {
-          require('dropbar.bar').dropbar_symbol_t:new(entry),
+          bar.dropbar_symbol_t:new(entry),
         },
       })
     end)
     :totable()
 end
 
+---@param buf integer buffer handler
+---@param win integer window handler
+---@return dropbar_symbol_t[]
 local function get_symbols(buf, win)
-  if not initialized then
-    init()
-  end
-
   local symbol = buf_info(buf)
+  ---@param self dropbar_symbol_t
   symbol.on_click = function(self)
     local entries = get_menu_entries(self)
     if #entries > 0 then
-      self.menu = require('dropbar.menu').dropbar_menu_t:new({
+      self.menu = menu.dropbar_menu_t:new({
         entries = entries,
         prev_win = self.bar.win,
       })
       self.menu:open({
         win_configs = {
           win = win,
-          col = function(menu)
-            if menu.prev_menu then
-              return menu.prev_menu._win_configs.width
+          col = function(this)
+            if this.prev_menu then
+              return this.prev_menu._win_configs.width
             end
             local mouse = vim.fn.getmousepos()
-            local bar = require('dropbar.api').get_dropbar(
-              vim.api.nvim_win_get_buf(menu.prev_win),
-              menu.prev_win
-            )
-            if not bar then
+            local winbar = utils.bar.get({ win = this.prev_win })
+            if not winbar then
               return 0
             end
             local _, range =
-              bar:get_component_at(math.max(0, mouse.wincol - 1))
+              winbar:get_component_at(math.max(0, mouse.wincol - 1))
             return range and range.start or 0
           end,
         },
@@ -83,9 +71,7 @@ local function get_symbols(buf, win)
     end
   end
 
-  return {
-    require('dropbar.bar').dropbar_symbol_t:new(symbol),
-  }
+  return { bar.dropbar_symbol_t:new(symbol) }
 end
 
 return { get_symbols = get_symbols }
