dc11786bc5a57d9317ab70b3bffa5f480f95816d
bekaboo <18127878294@qq.com>
2023-09-27 13:24:01 +0800

    fix(menu-scrollbar): scrollbar not at bottom when last line is shown (partially fix #93)

--- lua/dropbar/menu.lua
+++ lua/dropbar/menu.lua
@@ -531,12 +531,12 @@ function dropbar_menu_t:update_scrollbar()
     return
   end
 
-  local topline = vim.fn.line('w0')
   local thumb_height = math.max(1, math.floor(win_height ^ 2 / buf_height))
-  local offset = math.min(
-    win_height - thumb_height,
-    math.floor(win_height * topline / buf_height)
-  )
+  local offset = vim.fn.line('w$') == buf_height and win_height - thumb_height
+    or math.min(
+      win_height - thumb_height,
+      math.floor(win_height * vim.fn.line('w0') / buf_height)
+    )
 
   if self.scrollbar and vim.api.nvim_win_is_valid(self.scrollbar.thumb) then
     local config = vim.api.nvim_win_get_config(self.scrollbar.thumb)
