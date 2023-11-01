fc4396f366f133a726572b90a29f34d3fa0d5542
bekaboo <18127878294@qq.com>
2023-11-01 17:21:38 +0800

    refactor(sources-path): clean-up long lua table indexing etc

--- lua/dropbar/sources/path.lua
+++ lua/dropbar/sources/path.lua
@@ -7,18 +7,19 @@ local bar = require('dropbar.bar')
 ---@return string? icon_hl
 ---@return string? name_hl
 local function get_icon_and_hl(path)
-  local icon = configs.opts.icons.kinds.symbols.File
+  local icon_kind_opts = configs.opts.icons.kinds
+  local icon = icon_kind_opts.symbols.File
   local icon_hl = 'DropBarIconKindFile'
   local name_hl = 'DropBarKindFile'
   local stat = vim.loop.fs_stat(path)
   if not stat then
     return icon, icon_hl
   elseif stat.type == 'directory' then
-    icon = configs.opts.icons.kinds.symbols.Folder
+    icon = icon_kind_opts.symbols.Folder
     icon_hl = 'DropBarIconKindFolder'
     name_hl = 'DropBarKindFolder'
   end
-  if configs.opts.icons.kinds.use_devicons then
+  if icon_kind_opts.use_devicons then
     local devicons_ok, devicons = pcall(require, 'nvim-web-devicons')
     if devicons_ok and stat and stat.type ~= 'directory' then
       local devicon, devicon_hl = devicons.get_icon(
@@ -39,6 +40,7 @@ end
 ---@param win integer window handler
 ---@return dropbar_symbol_t
 local function convert(path, buf, win)
+  local path_opts = configs.opts.sources.path
   local icon, icon_hl, name_hl = get_icon_and_hl(path)
   return bar.dropbar_symbol_t:new(setmetatable({
     buf = buf,
@@ -57,7 +59,7 @@ local function convert(path, buf, win)
       if k == 'children' then
         self.children = {}
         for name in vim.fs.dir(path) do
-          if configs.opts.sources.path.filter(name) then
+          if path_opts.filter(name) then
             table.insert(self.children, convert(path .. '/' .. name, buf, win))
           end
         end
@@ -68,7 +70,7 @@ local function convert(path, buf, win)
         self.siblings = {}
         self.sibling_idx = 1
         for idx, name in vim.iter(vim.fs.dir(parent_dir)):enumerate() do
-          if configs.opts.sources.path.filter(name) then
+          if path_opts.filter(name) then
             table.insert(
               self.siblings,
               convert(parent_dir .. '/' .. name, buf, win)
@@ -90,23 +92,21 @@ end
 ---@param _ integer[] cursor position, ignored
 ---@return dropbar_symbol_t[] dropbar symbols
 local function get_symbols(buf, win, _)
+  local path_opts = configs.opts.sources.path
   local symbols = {} ---@type dropbar_symbol_t[]
-  local current_path = vim.fs.normalize(
-    vim.fn.fnamemodify((vim.api.nvim_buf_get_name(buf)), ':p')
-  )
+  local current_path = vim.fs.normalize((vim.api.nvim_buf_get_name(buf)))
+  local root = vim.fs.normalize(configs.eval(path_opts.relative_to, buf, win))
   while
-    current_path ~= '.'
+    current_path
+    and current_path ~= '.'
     and current_path ~= '/'
-    and current_path
-      ~= vim.fs.normalize(
-        configs.eval(configs.opts.sources.path.relative_to, buf, win)
-      )
+    and current_path ~= root
   do
     table.insert(symbols, 1, convert(current_path, buf, win))
     current_path = vim.fs.dirname(current_path)
   end
   if vim.bo[buf].mod then
-    symbols[#symbols] = configs.opts.sources.path.modified(symbols[#symbols])
+    symbols[#symbols] = path_opts.modified(symbols[#symbols])
   end
   return symbols
 end
