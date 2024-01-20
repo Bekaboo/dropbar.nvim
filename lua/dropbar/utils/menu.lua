0c3b4f6b64af8e24c115f5f4fd7b35c978ee43db
bekaboo <18127878294@qq.com>
2024-01-20 15:37:30 +0800

    feat(utils): menu: improve `ui.select()` function

--- lua/dropbar/utils/menu.lua
+++ lua/dropbar/utils/menu.lua
@@ -107,6 +107,20 @@ function M.select(items, opts, on_choice)
 
   opts = opts or {}
 
+  local bar = require('dropbar.bar')
+  local menu = require('dropbar.menu')
+  local configs = require('dropbar.configs')
+
+  -- Determine maximum width of the icon, for entries ranges from 1 to 9
+  -- they will be labeled and mapped with numbers, the rest will be labeled
+  -- and mapped with Meta + letter chosen from `pivots`, for even more items,
+  -- they will be labeled with number but no key mapped for them
+  local num_items = #items
+  local pivots = configs.opts.bar.pick.pivots
+  local len_pivots = #pivots
+  local num_bits = num_items == 1 and 1 or math.ceil(math.log10(num_items))
+  local icon_width = num_bits <= 1 and 1 or math.max(num_bits, #'M-a')
+  local icon_format = string.format('%%+%ds. ', icon_width)
   local entries = vim
     .iter(items)
     :enumerate()
@@ -114,19 +128,26 @@ function M.select(items, opts, on_choice)
       local text = item
       local virt_text
 
-      -- support custom formats for items like some
+      -- Support custom formats for items like some
       -- other ui-select plugins do
       if opts.format_item then
         text, virt_text = opts.format_item(item)
+        if type(virt_text) ~= 'table' then
+          virt_text = nil
+        end
       end
 
-      return require('dropbar.menu').dropbar_menu_entry_t:new({
-        -- virt_text will only be shown if returned from `format_item`
+      return menu.dropbar_menu_entry_t:new({
+        -- `virt_text` will only be shown if returned from `format_item`
         virt_text = virt_text,
         components = {
-          require('dropbar.bar').dropbar_symbol_t:new({
-            icon = ' ',
-            icon_hl = 'Special',
+          bar.dropbar_symbol_t:new({
+            icon = string.format(
+              icon_format,
+              (idx <= 9 or idx > 9 + len_pivots) and idx
+                or 'M-' .. pivots:sub(idx - 9, idx - 9)
+            ),
+            icon_hl = 'DropBarIconUIIndicator',
             name = text,
             on_click = function(self)
               self.entry.menu:close()
@@ -140,36 +161,65 @@ function M.select(items, opts, on_choice)
     end)
     :totable()
 
-  local border, title_pos
+  local win = vim.api.nvim_get_current_win()
+  local screenrow = vim.fn.screenpos(win, vim.fn.line('.'), 0).row
+  local screenrows_left = vim.go.lines - screenrow
+  local win_configs = {
+    col = 0,
+    relative = 'cursor',
+    title = opts.prompt,
+  }
+  -- Change border settings if the default top border is empty
+  -- to allow prompt to be displayed
   if opts.prompt then
-    border = require('dropbar.configs').opts.menu.win_configs.border
-    title_pos = 'center'
+    local border = configs.opts.menu.win_configs.border
+    local border_none_with_prompt = { '', ' ', '', '', '', '', '', '' }
+    if border == 'none' or border == 'shadow' then
+      win_configs.border = border_none_with_prompt
+    elseif type(border) == 'table' then
+      if #border == 1 and border[1] == '' then
+        win_configs.border = border_none_with_prompt
+      elseif #border > 1 and border[2] == '' then
+        local border_cp = vim.deepcopy(border)
+        border_cp[2] = ' '
+        win_configs.border = border_cp
+      end
+    end
+  end
+  -- Place menu above or below the cursor depending on the available
+  -- screen space
+  if screenrow > screenrows_left then
+    win_configs.row = 0
+    win_configs.anchor = 'SW'
+  else
+    win_configs.row = 1
+    win_configs.anchor = 'NW'
   end
 
-  local menu = require('dropbar.menu').dropbar_menu_t:new({
+  local smenu = menu.dropbar_menu_t:new({
+    prev_win = win,
     entries = entries,
-    prev_win = vim.api.nvim_get_current_win(),
-    win_configs = {
-      relative = 'cursor',
-      title = opts.prompt,
-      row = 1,
-      col = 1,
-      border = border,
-      title_pos = title_pos,
-    },
+    win_configs = win_configs,
   })
 
-  menu:open()
+  smenu:open()
 
-  vim.api.nvim_create_autocmd('CursorMoved', {
-    buffer = menu.buf,
-    callback = function()
-      local cursor = { vim.api.nvim_win_get_cursor(menu.win)[1], 1 }
-      vim.api.nvim_win_set_cursor(menu.win, cursor)
-      menu:update_hover_hl(cursor)
-    end,
-    desc = 'Lock cursor to the first column of the menu',
-  })
+  -- Set buffer-local keymaps
+  if smenu.buf and vim.api.nvim_buf_is_valid(smenu.buf) then
+    -- Press a number to go to the corresponding item
+    for i = 1, math.min(9, num_items) do
+      local i_str = tostring(i)
+      vim.keymap.set('n', i_str, function()
+        vim.cmd(i_str)
+      end, { buffer = smenu.buf })
+    end
+    -- Press Meta + letter to go to the corresponding item
+    for i = 1, math.min(len_pivots, num_items - 9) do
+      vim.keymap.set('n', string.format('<M-%s>', pivots:sub(i, i)), function()
+        vim.cmd(tostring(i + 9))
+      end, { buffer = smenu.buf })
+    end
+  end
 end
 
 return M
