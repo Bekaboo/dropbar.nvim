fa43ea3546f9e8a8fab5eecc18bce2cfd2946249
Yi Ming <ofseed@foxmail.com>
2024-02-16 06:54:27 +0800

    fix(autocmd): add missing setup event `BufNewFile` (#140)

--- plugin/dropbar.lua
+++ plugin/dropbar.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, {
+vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile', 'BufWritePost' }, {
   once = true,
   group = vim.api.nvim_create_augroup('DropBarSetup', {}),
   callback = function()
