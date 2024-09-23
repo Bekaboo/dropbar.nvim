b7b0cd2670cfab298afc955f2531206218736766
phanium <91544758+phanen@users.noreply.github.com>
2024-09-23 11:59:56 +0800

    fix(setup): avoid setup twice when using lazy.nvim (#181)

--- plugin/dropbar.lua
+++ plugin/dropbar.lua
@@ -2,6 +2,9 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile', 'BufWritePost' }, {
   once = true,
   group = vim.api.nvim_create_augroup('DropBarSetup', {}),
   callback = function()
+    if vim.g.loaded_dropbar then
+      return
+    end
     require('dropbar').setup()
   end,
 })
