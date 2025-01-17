e7542e73a0e60547631dc1a7eb9006be1c2d2737
bekaboo <kankefengjing@gmail.com>
2025-01-17 01:03:24 -0500

    test(init): clean up minimal init

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,19 +1,24 @@
 vim.opt.rtp:prepend('.')
-vim.opt.rtp:prepend(
-  unpack(vim.fs.find('plenary.nvim', { path = vim.fn.stdpath('data') }))
-    or unpack(vim.fs.find('plenary.nvim', { path = vim.fs.normalize('..') }))
-)
-vim.opt.rtp:prepend(
-  unpack(
-    vim.fs.find('telescope-fzf-native.nvim', { path = vim.fn.stdpath('data') })
-  )
-    or unpack(
-      vim.fs.find(
-        'telescope-fzf-native.nvim',
-        { path = vim.fs.normalize('..') }
-      )
-    )
-)
+
+local data_path = vim.fn.stdpath('data') --[[@as string]]
+local parent_path = vim.fs.normalize('..')
+
+---Add plugin with given name to nvim runtime path
+---@param name string
+local function add_to_rtp(name)
+  local plenary_path = unpack(vim.fs.find(name, { path = data_path }))
+    or unpack(vim.fs.find(name, { path = parent_path }))
+
+  if plenary_path then
+    vim.opt.rtp:prepend(plenary_path)
+  else
+    error(string.format('%s not found', name))
+  end
+end
+
+add_to_rtp('plenary.nvim')
+add_to_rtp('telescope-fzf-native.nvim')
+
 vim.cmd.luafile('tests/utils.lua')
 require('dropbar').setup()
 
