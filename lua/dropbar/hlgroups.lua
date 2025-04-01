da78f3ec7e0030eb0985efc1f74d309c714cd7e5
bekaboo <kankefengjing@gmail.com>
2025-04-01 12:19:50 -0400

    refactor!: drop neovim 0.10 support

--- lua/dropbar/hlgroups.lua
+++ lua/dropbar/hlgroups.lua
@@ -99,24 +99,6 @@ local function set_hlgroups()
   end
 end
 
----Set WinBar & WinBarNC background to Normal background
----@return nil
-local function clear_winbar_bg()
-  ---@param name string
-  ---@return nil
-  local function _clear_bg(name)
-    local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
-    if hl.bg or hl.ctermbg then
-      hl.bg = nil
-      hl.ctermbg = nil
-      vim.api.nvim_set_hl(0, name, hl)
-    end
-  end
-
-  _clear_bg('WinBar')
-  _clear_bg('WinBarNC')
-end
-
 ---Initialize highlight groups for dropbar
 local function init()
   local groupid = vim.api.nvim_create_augroup('DropBarHlGroups', {})
@@ -126,24 +108,6 @@ local function init()
     group = groupid,
     callback = set_hlgroups,
   })
-
-  -- Remove winbar background for non-nightly versions as a workaround for
-  -- https://github.com/Bekaboo/dropbar.nvim/issues/118, also see
-  -- https://github.com/neovim/neovim/issues/26037#issuecomment-1838548013
-  if vim.fn.has('nvim-0.11.0') == 0 then
-    clear_winbar_bg()
-    vim.api.nvim_create_autocmd('ColorScheme', {
-      desc = 'Remove WinBar background color as a workaround.',
-      group = groupid,
-      callback = clear_winbar_bg,
-    })
-    vim.api.nvim_create_autocmd('OptionSet', {
-      desc = 'Remove WinBar background color as a workaround.',
-      pattern = 'background',
-      group = groupid,
-      callback = clear_winbar_bg,
-    })
-  end
 end
 
 return {
