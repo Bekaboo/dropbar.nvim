e2154ba25ebe5aa3c4b1abe4ad0ad7c1190a036a
bekaboo <kankefengjing@gmail.com>
2025-05-07 14:18:56 -0700

    tests: ensure test dependencies in `Makefile`

--- Makefile
+++ Makefile
@@ -3,6 +3,18 @@ all: format-check lint test
 
 .PHONY: test
 test:
+	# Install test dependencies
+	@if [ ! -e ../plenary.nvim ]; then \
+		git -C .. clone --depth=1 --filter=blob:none \
+			https://github.com/nvim-lua/plenary.nvim; \
+	fi
+	@if [ ! -e ../telescope-fzf-native.nvim ]; then \
+		git -C .. clone --depth=1 --filter=blob:none \
+			https://github.com/nvim-telescope/telescope-fzf-native.nvim; \
+		(cd ../telescope-fzf-native.nvim && make); \
+	fi
+
+	# Run test
 	nvim --headless -u tests/minimal_init.lua \
 		-c "PlenaryBustedDirectory tests/ \
 			{ minimal_init = 'tests/minimal_init.lua' }"
--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,25 +1,8 @@
-vim.opt.rtp:prepend('.')
-
-local data_path = vim.fn.stdpath('data') --[[@as string]]
-local parent_path = vim.fn.fnamemodify('..', ':p')
-
----Add plugin with given name to nvim runtime path
----@param name string
-local function add_to_rtp(name)
-  local plugin_path = unpack(vim.fs.find(name, { path = data_path }))
-    or unpack(vim.fs.find(name, { path = parent_path }))
-
-  if plugin_path then
-    vim.opt.rtp:prepend(plugin_path)
-  else
-    error(string.format('%s not found', name))
-  end
-end
-
-add_to_rtp('plenary.nvim')
-add_to_rtp('telescope-fzf-native.nvim')
-
-vim.cmd.luafile('tests/utils.lua')
-require('dropbar').setup()
-
 vim.o.swapfile = false
+
+vim.opt.rtp:prepend('.')
+vim.opt.rtp:prepend('../plenary.nvim')
+vim.opt.rtp:prepend('../telescope-fzf-native.nvim')
+
+dofile('tests/utils.lua')
+require('dropbar').setup()
