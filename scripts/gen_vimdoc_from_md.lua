71d3c8b1c5bb7e077160e624c3d987d355fa0f3d
bekaboo <kankefengjing@gmail.com>
2025-05-12 12:44:28 -0700

    docs: use '=' for lev 2 section headings in vimdoc

--- scripts/gen_vimdoc_from_md.lua
+++ scripts/gen_vimdoc_from_md.lua
@@ -3,6 +3,14 @@ require('nvim-treesitter.install').commands.TSInstallSync['run!'](
   'markdown_inline'
 )
 
+require('ts-vimdoc.formatting').style_elements = {
+  header_break = {
+    string.rep('=', 78), -- atx_h1_marker
+    string.rep('=', 78), -- atx_h2_marker
+    string.rep('-', 78), -- atx_h3_marker
+  },
+}
+
 require('ts-vimdoc').docgen({
   input_file = 'README.md',
   output_file = 'doc/dropbar.txt',
