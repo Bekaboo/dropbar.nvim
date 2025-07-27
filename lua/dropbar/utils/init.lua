f5addbb9c5f625f7d0e5e1b6a90b470a1d71216a
bekaboo <kankefengjing@gmail.com>
2025-07-26 18:10:50 -0700

    chore(utils): properly annotate `utils` submodules

--- lua/dropbar/utils/init.lua
+++ lua/dropbar/utils/init.lua
@@ -1,4 +1,8 @@
-return setmetatable({}, {
+return setmetatable({
+  bar = nil, ---@module 'dropbar.utils.bar'
+  menu = nil, ---@module 'dropbar.utils.menu'
+  source = nil, ---@module 'dropbar.utils.source'
+}, {
   __index = function(_, key)
     return require('dropbar.utils.' .. key)
   end,
